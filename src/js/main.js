0db2dccbe1469929ec82b9672caaa77a2ee86633
Karina <avrora.shp@mail.ru>
2023-06-08 15:22:45 +0300

    adaptive part two

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,29 @@ $(function () {
     $('.review__slider').slick({
         dots: true,
         prevArrow: '<button type="button" class="arrowleft"><img src="../img/icons/left-arrow.svg" alt="arrow left"></button>',
-        nextArrow: '<button type="button" class="arrowright"><img src = "../img/icons/right-arrow.svg" alt = "arrow right"></button>'
+        nextArrow: '<button type="button" class="arrowright"><img src = "../img/icons/right-arrow.svg" alt = "arrow right"></button>',
+        responsive: [
+            {
+                breakpoint: 876,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
     $('.bestsellers__catalog').slick({
         dots: false,
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: '<button type="button" class="bestsellers-arrow-left arrowleft"><img src="../img/icons/left-arrow.svg" alt="arrow left"></button>',
-        nextArrow: '<button type="button" class="bestsellers-arrow-right arrowright"><img src = "../img/icons/right-arrow.svg" alt = "arrow right"></button>'
+        nextArrow: '<button type="button" class="bestsellers-arrow-right arrowright"><img src = "../img/icons/right-arrow.svg" alt = "arrow right"></button>',
+   /*     responsive: [
+            {
+                breakpoint: 1350,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]*/
     });
 })
